68c46b5225db049cc660a9c3a4579279b7582abf
Joao Barbosa <joao.barbosa@scalors.com>
2016-02-18 10:08:13 +0000

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+@toc
+1. setup - whitelist, appPath, html5Mode
+*/
+
+'use strict';
+
+angular.module('myApp', [
+'ngRoute', 'ngSanitize', 'ngTouch', 'ngAnimate',		//additional angular modules
+'barbosajlm.ng-rome'
+]).
+config(['$routeProvider', '$locationProvider', '$compileProvider', function($routeProvider, $locationProvider, $compileProvider) {
+	/**
+	setup - whitelist, appPath, html5Mode
+	@toc 1.
+	*/
+	$locationProvider.html5Mode(false);		//can't use this with github pages / if don't have access to the server
+	
+	// var staticPath ='/';
+	var staticPath;
+	// staticPath ='/angular-directives/ng-rome/';		//local
+	staticPath ='/';		//nodejs (local)
+	// staticPath ='/ng-rome/';		//gh-pages
+	var appPathRoute ='/';
+	var pagesPath =staticPath+'pages/';
+	
+	
+	$routeProvider.when(appPathRoute+'home', {templateUrl: pagesPath+'home/home.html'});
+
+	$routeProvider.otherwise({redirectTo: appPathRoute+'home'});
+	
+}]);
